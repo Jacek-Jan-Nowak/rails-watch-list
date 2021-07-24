d53f6b3919a21ea81c89e3a2db72fb3aa626f3d6
Jacek Nowak <jacek.jan.nowak@gmail.com>
2021-07-24 13:19:24 +0100

    photo uploading

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,8 @@
 class List < ApplicationRecord
+  has_one_attached :photo
   has_many :bookmarks
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   has_many :movies, through: :bookmarks, dependent: :destroy
   validates :name, presence: true, uniqueness: true
+  validates :photo, presence: true
 end
